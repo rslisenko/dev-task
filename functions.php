4731e1e3028539b504891cca523fe94b19a181c1
rslisenko <rvslisenko@gmail.com>
2023-03-29 18:33:06 +0300

    disable admin bar for wp-test user

--- functions.php
+++ functions.php
@@ -12,3 +12,21 @@ function enqueue_scripts_and_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( $parenthandle ), $theme->get( 'Version' ));
 
 }
+
+/**
+*   Disable admin bar for a user
+ */
+add_action('after_setup_theme', 'remove_admin_bar');
+function remove_admin_bar() {
+    if (is_user_logged_in()) {
+
+        $user = wp_get_current_user();
+
+        if ($user->data->user_login == 'wp-test') {
+            add_filter('show_admin_bar', '__return_false');
+        }
+
+    }
+
+}
+
